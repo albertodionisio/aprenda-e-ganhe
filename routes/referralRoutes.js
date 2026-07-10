9b5cc609637ebf6ab39b7276383d61554e58f2f8
albertodionisio <albertodionisio548@gmail.com>
2026-07-10 15:15:03 +0200

    Delete routes/referralRoutes.js

--- routes/referralRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ReferralController = require('../app/Controllers/ReferralController');
-const { protectRoute } = require('../app/Middleware/authMiddleware');
-
-router.get('/', protectRoute, ReferralController.getMyReferrals);
-
-module.exports = router;
