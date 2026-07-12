e7565655d90983d77541f63d412b15332e5e6fa3
albertodionisio <albertodionisio548@gmail.com>
2026-07-12 14:29:59 +0200

    Update quizRoutes.js

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -7,6 +7,7 @@ const antiFraudCheck = require('../app/Middleware/antiFraud');
 router.get('/categories', protectRoute, QuizController.listCategories);
 router.get('/next', protectRoute, QuizController.getRandomQuiz);
 router.post('/submit', protectRoute, antiFraudCheck, QuizController.submitAnswer);
+router.post('/ad-boost', protectRoute, QuizController.adBoost);
 router.post('/add', protectRoute, adminOnly(), QuizController.addQuiz);
 
 module.exports = router;
